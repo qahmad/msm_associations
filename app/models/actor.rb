8876041d9b0579589cd3798e2df6841c8e0b76a0
Saud <qahmad@chicagobooth.edu>
2017-08-02 02:50:50 +0000

    First check in

---
+++ app/models/actor.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: actors
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  dob        :string
+#  bio        :text
+#  image_url  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Actor < ApplicationRecord
+    
+#     Actor:
+#  - name: must be present; must be unique in combination with dob
+validates :name, :presence => true, :uniqueness => { :scope => :dob }
+
+#  - dob: no rules
+#  - bio: no rules
+#  - image_url: no rules
+
+    
+end
